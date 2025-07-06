a8e83958cecd8f0a23044b69408d54a39890d307
Nathan <test@gmail.com>
2025-07-05 23:48:32 -0400

    drag and drop into terms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,11 @@ import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
-
 import SideBar from "~/app/_components/sidebar";
 import Planner from "~/app/_components/planner";
 
 export default async function Home() {
-  const hello = await api.planner.hello({ text: "from f" });
+  // const hello = await api.planner.hello({ text: "from f" });
   const session = await auth();
 
   if (session?.user) {
