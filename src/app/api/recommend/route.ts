954f17242e436f5eb7d576739233191e5ea1ff61
Nathan <n25jiang@gmail.com>
2025-07-27 21:47:21 -0400

    recommendation route final

--- src/app/api/recommend/route.ts
+++ src/app/api/recommend/route.ts
@@ -7,26 +7,6 @@ export async function GET(
 ) {
   try {
 
-    // Select courses taken DONE 
-    // Get courses that have all preqeqs satisfied (build tree from bottom up)
-          // Filter by department DONE
-          // Start with the prereq nodes for courses taken
-          // Recursively select AND nodes with all children in the set, OR nodes with a child in the set
-    
-          // Include courses without prereqs
-     
-    // Get prereq tree top down
-    // const result = await db.execute(`
-    //   WITH RECURSIVE tmp as
-    //   (
-    //     SELECT pn.department, pn.course_number, pn.id, pn.parent_id, pn.relation_type FROM prerequisite_nodes pn
-    //     WHERE pn.id = (SELECT root_node_id FROM course_prerequisites WHERE department = 'MATH' AND course_number = '239')
-    //     UNION
-    //     SELECT pn.department, pn.course_number, pn.id, pn.parent_id, pn.relation_type FROM prerequisite_nodes pn JOIN tmp ON pn.parent_id = tmp.id
-    //   ) 
-    //   SELECT * FROM tmp
-    // `);
-
     const result = await db.execute(`
       WITH courses_taken(department, course_number) as 
       (SELECT department, course_number FROM user_courses WHERE user_id = 'master'),
@@ -47,36 +27,6 @@ export async function GET(
       SELECT department, course_number FROM root_nodes JOIN course_prerequisites ON root_node_id = id
     `)
     
-    // For full correctness, query to get the base case of the prereq tree
-
-    // WITH courses_taken(department, course_number) as 
-    // (SELECT department, course_number FROM user_courses WHERE user_id = 'master'),
-    // SELECT p.id, p.parent_id, p.relation_type 
-    // FROM prerequisite_nodes p JOIN courses_taken ct ON p.department = ct.department AND p.course_number = ct.course_number
-
-    // Then, with these nodes, repeatedly build the prereq tree
-
-    // SELECT p.id, p.parent_id, p.relation_type  
-    // FROM prerequisite_nodes 
-    // WHERE p.relation_type = 'OR' AND 
-    // OR 
-
-
-    // SELECT c.department, c.course_number 
-    //   FROM courses c
-    //   WHERE c.department in (SELECT department FROM courses_taken)
-    //   AND
-    //   EXISTS
-    //   (
-    //     WITH RECURSIVE tmp as
-    //     (
-    //       SELECT pn.department, pn.course_number, pn.id FROM prerequisite_nodes pn
-    //       WHERE pn.id = (SELECT root_node_id FROM course_prerequisites WHERE department = c.department AND course_number = c.course_number)
-    //       UNION
-    //       SELECT pn.department, pn.course_number, pn.id FROM prerequisite_nodes pn JOIN tmp ON pn.parent_id = tmp.id
-    //     ) 
-    //     SELECT t.department, t.course_number FROM tmp t JOIN courses_taken ct ON ct.department = t.department AND ct.course_number = t.course_number
-    //   )
       
     
     return NextResponse.json(result);
